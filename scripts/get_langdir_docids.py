9cfa2d784dd1d9bd1ec009e5d6f7fef91331c066
Rachel Bawden <rachel.bawden@inria.fr@users.noreply.github.com>
2022-12-27 10:56:59 +0100

    added script to get diabla language direction

---
+++ scripts/get_langdir_docids.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import json
+import argparse
+import re
+
+parser = argparse.ArgumentParser()
+parser.add_argument('xglm_jsonl')
+parser.add_argument('src_lang', choices=('English', 'French'))
+args = parser.parse_args()
+
+with open(args.xglm_jsonl) as fp:
+    for line in fp:
+        ex = json.loads(line)
+        if re.match('.+?###\n(English|French)', ex['ctx'], re.DOTALL)[1] == args.src_lang:
+            print(json.dumps(ex))
