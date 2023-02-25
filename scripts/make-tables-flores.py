de7140e3a8e5217361f6a9bd5faa456ee165cf6b
Rachel Bawden <rachel.bawden@inria.fr@users.noreply.github.com>
2023-02-25 23:58:40 +0100

    added all latex tables for flores

--- scripts/make-tables-flores.py
+++ scripts/make-tables-flores.py
@@ -6,7 +6,7 @@ lang2code = {'Galician': 'gl', 'Latin American Spanish': 'es', 'Brazilian Portug
 
 # load the results files (tsv format) as a dataframe
 def load_df(filename, model, postproc, task):
-    data = pd.read_csv(results_file, delimiter='\t', index_col=False)
+    data = pd.read_csv(filename, delimiter='\t', index_col=False)
     data = data[data['model'] == model] 
     data = data[data['task'] == task]
     data = data.fillna('')
@@ -16,14 +16,12 @@ def load_df(filename, model, postproc, task):
 # table of high-resource MT results
 def table_highresource(results_file, model='bloom', postproc=''):
     data = load_df(results_file, model, postproc, 'flores_101_mt')
-
-    # M2M scores
     m2m = {'ar': {'en': 25.50, 'es': 16.74, 'fr': 25.69, 'zh': 13.10},
            'en': {'ar': 17.92, 'es': 25.57, 'fr': 41.99, 'zh': 19.33},
            'es': {'ar': 12.11, 'en': 25.09, 'fr': 29.33, 'zh': 14.86},
            'fr': {'ar': 15.36, 'en': 37.17, 'es': 25.60, 'zh': 17.61},
            'zh': {'ar': 11.55, 'en': 20.91, 'es': 16.92, 'fr': 24.32}}
-    
+
     # store results
     langs = ['English', 'Arabic', 'French', 'simplified Chinese', 'Latin American Spanish']
     lang2lang = {}
@@ -33,7 +31,7 @@ def table_highresource(results_file, model='bloom', postproc=''):
         for lang2 in langs:
             lg2 = lang2code.get(lang2, lang2[:2].lower())
             if lang1 != lang2:
-                result = data[data['template'] == 'flores-xglm-' + lang1 + '-' + lang2]
+                result = data[data['template'] == 'xglm-' + lang1 + '-' + lang2 + '-source+target']
                 assert len(result) <= 1
                 if len(result) == 0:
                     lang2lang[lg1][lg2] = '--'
@@ -42,98 +40,29 @@ def table_highresource(results_file, model='bloom', postproc=''):
             else:
                 lang2lang[lg1][lg2] = '--'
                 
-    # get table
-    print(r'Src \ Trg & ' + ' & '.join(sorted(lang2lang)) + r' \\')
-    print(r'\midrule')
+    # print latex table
+    precision = 4 if 'comet' in results_file else 2
+    print(r'\scalebox{0.85}{')
+    print(r'\begin{tabular}{llrrrrr}')
+    print(r'\toprule')
+    print(r'Src $\downarrow$ & Trg $\rightarrow$ & ' + ' & '.join(sorted(lang2lang)) + r' \\')
     for lang in sorted(lang2lang):
-        results = ["%.2f" % round(lang2lang[lang][trg], 2) if lang2lang[lang][trg] != '--' else '--' for trg in sorted(lang2lang)]
-        print('\multirow{2}{*}{' + lang + r'} & \bloom & ' + ' & '.join(results) + r' \\')
         print(r'\midrule')
-    print('\n\n')
-
-# table of Romance lang MT results
-def table_romance_langs(results_file, model='bloom', postproc=''):
-    data = load_df(results_file, model, postproc, 'flores_101_mt')
-
-    m2m = {'ca': {'es': 25.17, 'fr': 35.08, 'gl': 33.42, 'it': 25.50, 'pt': 35.17},
-           'es': {'ca': 23.12, 'fr': 29.33, 'gl': 27.54, 'it': 23.87, 'pt': 28.10},
-           'fr': {'ca': 28.74, 'es': 25.60, 'gl': 32.82, 'it': 28.56, 'pt': 37.84},
-           'gl': {'ca': 30.07, 'es': 27.65, 'fr': 37.06, 'it': 26.87, 'pt': 34.81},
-           'it': {'ca': 25.20, 'es': 29.23, 'fr': 34.39, 'gl': 29.23, 'pt': 31.47},
-           'pt': {'ca': 30.69, 'es': 26.88, 'fr': 40.17, 'gl': 33.77, 'it': 28.09}}
-    
-    # store results
-    langs = ['Catalan', 'Latin American Spanish', 'French', 'Galician', 'Brazilian Portuguese', 'Italian']
-    lang2lang = {}
-    for lang1 in langs:
-        lg1 = lang2code.get(lang1, lang1[:2].lower())
-        lang2lang[lg1] = {}
-        for lang2 in langs:
-            lg2 = lang2code.get(lang2, lang2[:2].lower())
-            if lang1 != lang2:
-                result = data[data['template'] == 'flores-xglm-' + lang1 + '-' + lang2]
-                assert len(result) <= 1
-                if len(result) == 0:
-                    lang2lang[lg1][lg2] = '--'
-                else:
-                    lang2lang[lg1][lg2] = result['spBLEU'].values[0]
-            else:
-                lang2lang[lg1][lg2] = '--'
-                
-    # get table
-    print(r'Src \ Trg & ' + ' & '.join(sorted(lang2lang)) + r' \\')
-    print(r'\midrule')
-    for lang in sorted(lang2lang):
-        results = ["%.2f" % round(lang2lang[lang][trg], 2) if lang2lang[lang][trg] != '--' else '--' for trg in sorted(lang2lang)]
+        results = [f"{round(lang2lang[lang][trg], precision):{precision}.{precision}f}" if lang2lang[lang][trg] != '--' else '--' for trg in sorted(lang2lang)]
+        m2m_results = [f"{round(m2m[lang][trg], precision):{precision}.{precision}f}" if trg in m2m[lang]  else '--' for trg in sorted(lang2lang)]
         print('\multirow{2}{*}{' + lang + r'} & \bloom & ' + ' & '.join(results) + r' \\')
-        print(r'\midrule')
-    print('\n\n')
+        print(r' & M2M & ' + ' & '.join(m2m_results) + r' \\')
 
-# table of low-resource MT results
-def table_lowresource(results_file, model='bloom', postproc=''):
-    data = load_df(results_file, model, postproc, 'flores_101_mt')
-
-
-    m2m = {'en': {'bn': 23.04, 'hi': 28.15, 'sw': 26.95, 'yo': 2.17},
-           'bn': {'en': 22.86, 'hi': 21.76},
-           'hi': {'en': 27.89, 'bn': 21.77},
-           'sw': {'en': 30.43, 'yo': 1.29},
-           'yo': {'en': 4.18, 'sw': 1.93}}
-    
-    #print(data[data.duplicated(keep=False)]) # check duplicated
-    # store results                                                                                                                                  
-    langs = ['English', 'Bengali', 'Hindi', 'Swahili', 'Yoruba']
-    lang2lang = {}
-    for lang1 in langs:
-        lg1 = lang2code.get(lang1, lang1[:2].lower())
-        lang2lang[lg1] = {}
-        for lang2 in langs:
-            lg2 = lang2code.get(lang2, lang2[:2].lower())
-            if lang1 != lang2:
-                result = data[data['template'] == 'flores-xglm-' + lang1 + '-' + lang2]
-                assert len(result) <= 1
-                if len(result) == 0:
-                    lang2lang[lg1][lg2] = '--'
-                else:
-                    lang2lang[lg1][lg2] = result['spBLEU'].values[0]
-            else:
-                lang2lang[lg1][lg2] = '--'
-
-    # get table
-    lang_codes = [lang2code.get(x, x[:2].lower()) for x in langs]
-    print(r'Src \ Trg & ' + ' & '.join(lang_codes) + r' \\')
-    print(r'\midrule')
-    for lang in lang_codes:
-        results = ["%.2f" % round(lang2lang[lang][trg], 2) if lang2lang[lang][trg] != '--' else '--' for trg in lang_codes]
-        print('\multirow{2}{*}{' + lang + r'} & \bloom & ' + ' & '.join(results) + r' \\')
-        print(r'\midrule')
+    print(r'\bottomrule')
+    print(r'\end{tabular}}')
+    print(r'\caption{High-resource language pairs.}')
+    print(r'\label{tab:flores101_summary:high-high}')
     print('\n\n')
 
 
 # table of mid-high-resource MT results
 def table_midresource(results_file, model='bloom', postproc=''):
     data = load_df(results_file, model, postproc, 'flores_101_mt')
-
     m2m = {'en': {'fr': 41.99, 'hi': 28.15, 'id': 37.26,'vi': 35.10},
            'fr': {'en': 37.17 ,'hi': 22.91, 'id': 29.14,'vi': 30.26},
            'hi': {'en': 27.89, 'fr': 25.88},
@@ -150,7 +79,7 @@ def table_midresource(results_file, model='bloom', postproc=''):
         for lang2 in langs:
             lg2 = lang2code.get(lang2, lang2[:2].lower())
             if lang1 != lang2:
-                result = data[data['template'] == 'flores-xglm-' + lang1 + '-' + lang2]
+                result = data[data['template'] == 'flores-xglm-' + lang1 + '-' + lang2 + '-source+target']
                 assert len(result) <= 1
                 if len(result) == 0:
                     lang2lang[lg1][lg2] = '--'
@@ -160,18 +89,183 @@ def table_midresource(results_file, model='bloom', postproc=''):
                 lang2lang[lg1][lg2] = '--'
 
     # get table
+    precision = 4 if 'comet' in results_file else 2
+    print(r'\scalebox{0.85}{')
+    print(r'\begin{tabular}{llrrrrr}')
+    print(r'\toprule')
     lang_codes = [lang2code.get(x, x[:2].lower()) for x in langs]
-    print(r'Src \ Trg & ' + ' & '.join(lang_codes) + r' \\')
-    print(r'\midrule')
+    print(r'Src $\downarrow$ & Trg $\rightarrow$ & ' + ' & '.join(lang_codes) + r' \\')
+
     for lang in lang_codes:
-        results = ["%.2f" % round(lang2lang[lang][trg], 2) if lang2lang[lang][trg] != '--' else '--' for trg in lang_codes]
-        print('\multirow{2}{*}{' + lang + r'} & \bloom & ' + ' & '.join(results) + r' \\')
         print(r'\midrule')
+        results = [f"{round(lang2lang[lang][trg], precision):{precision}.{precision}f}" if lang2lang[lang][trg] != '--' else '--' for trg in lang_codes]
+        m2m_results = [f"{round(m2m[lang][trg], precision):{precision}.{precision}f}" if trg in m2m[lang]  else '--' for trg in sorted(lang2lang)]
+        print('\multirow{2}{*}{' + lang + r'} & \bloom & ' + ' & '.join(results) + r' \\')
+        print(r' & M2M & ' + ' & '.join(m2m_results) + r' \\')
+    print(r'\bottomrule')
+    print(r'\end{tabular}}')
+    print(r'\caption{High$\rightarrow$mid-resource language pairs.}')
+    print(r'\label{tab:flores101:high-mid}')
     print('\n\n')
 
+    
+# table of low-resource MT results
+def table_lowresource(results_file, model='bloom', postproc=''):
+    data = load_df(results_file, model, postproc, 'flores_101_mt')
+    m2m = {'en': {'bn': 23.04, 'hi': 28.15, 'sw': 26.95, 'yo': 2.17},
+           'bn': {'en': 22.86, 'hi': 21.76},
+           'hi': {'en': 27.89, 'bn': 21.77},
+           'sw': {'en': 30.43, 'yo': 1.29},
+           'yo': {'en': 4.18, 'sw': 1.93}}
+    
+    #print(data[data.duplicated(keep=False)]) # check duplicated
+    # store results                                                                                                                                  
+    langs = ['English', 'Bengali', 'Hindi', 'Swahili', 'Yoruba']
+    lang2lang = {}
+    for lang1 in langs:
+        lg1 = lang2code.get(lang1, lang1[:2].lower())
+        lang2lang[lg1] = {}
+        for lang2 in langs:
+            lg2 = lang2code.get(lang2, lang2[:2].lower())
+            if lang1 != lang2:
+                result = data[data['template'] == 'xglm-' + lang1 + '-' + lang2 + '-source+target']
+                assert len(result) <= 1
+                if len(result) == 0:
+                    lang2lang[lg1][lg2] = '--'
+                else:
+                    lang2lang[lg1][lg2] = result['spBLEU'].values[0]
+            else:
+                lang2lang[lg1][lg2] = '--'
+
+    # get latex table
+    precision = 4 if 'comet' in results_file else 2
+    lang_codes = [lang2code.get(x, x[:2].lower()) for x in langs]
+    print(r'\scalebox{0.86}{')
+    print(r'\begin{tabular}{llrrrrr}')
+    print(r'\toprule')
+    print(r'Src$\downarrow$ & Trg$\rightarrow$ & ' + ' & '.join(lang_codes) + r' \\')
+    for lang in lang_codes:
+        print(r'\midrule')
+        results = [f"{round(lang2lang[lang][trg], precision):{precision}.{precision}f}" if lang2lang[lang][trg] != '--' else '--' for trg in lang_codes]
+        m2m_results = [f"{round(m2m[lang][trg], precision):{precision}.{precision}f}" if trg in m2m[lang]  else '--' for trg in lang_codes]
+        print('\multirow{2}{*}{' + lang + r'} & \bloom & ' + ' & '.join(results) + r' \\')
+        print(r' & M2M & ' + ' & '.join(m2m_results) + r' \\')
+
+    print(r'\bottomrule')
+    print(r'\end{tabular}}')
+    print(r'\caption{Low-resource languages}')
+    print(r'\label{tab:flores101_summary:high-low}')
+    print('\n\n')
+
+
+# table of Romance lang MT results
+def table_romance_langs(results_file, model='bloom', postproc=''):
+    data = load_df(results_file, model, postproc, 'flores_101_mt')
+    m2m = {'ca': {'es': 25.17, 'fr': 35.08, 'gl': 33.42, 'it': 25.50, 'pt': 35.17},
+           'es': {'ca': 23.12, 'fr': 29.33, 'gl': 27.54, 'it': 23.87, 'pt': 28.10},
+           'fr': {'ca': 28.74, 'es': 25.60, 'gl': 32.82, 'it': 28.56, 'pt': 37.84},
+           'gl': {'ca': 30.07, 'es': 27.65, 'fr': 37.06, 'it': 26.87, 'pt': 34.81},
+           'it': {'ca': 25.20, 'es': 29.23, 'fr': 34.39, 'gl': 29.23, 'pt': 31.47},
+           'pt': {'ca': 30.69, 'es': 26.88, 'fr': 40.17, 'gl': 33.77, 'it': 28.09}}
+    
+    # store results
+    langs = ['Catalan', 'Latin American Spanish', 'French', 'Galician', 'Brazilian Portuguese', 'Italian']
+    lang2lang = {}
+    for lang1 in langs:
+        lg1 = lang2code.get(lang1, lang1[:2].lower())
+        lang2lang[lg1] = {}
+        for lang2 in langs:
+            lg2 = lang2code.get(lang2, lang2[:2].lower())
+            if lang1 != lang2:
+                result = data[data['template'] == 'xglm-' + lang1 + '-' + lang2 + '-source+target']
+                assert len(result) <= 1
+                if len(result) == 0:
+                    lang2lang[lg1][lg2] = '--'
+                else:
+                    lang2lang[lg1][lg2] = result['spBLEU'].values[0]
+            else:
+                lang2lang[lg1][lg2] = '--'
+                
+    # print latex table
+    precision = 4 if 'comet' in results_file else 2
+    print(r'\resizebox{\linewidth}{!}{')
+    print(r'\begin{tabular}{llrrrrrr}')
+    print(r'\toprule')
+    print(r'Src$\downarrow$ & Trg$\rightarrow$ & ' + ' & '.join(sorted(lang2lang)) + r' \\')
+    for lang in sorted(lang2lang):
+        print(r'\midrule')
+        results = [f"{round(lang2lang[lang][trg], precision):{precision}.{precision}f}" if lang2lang[lang][trg] != '--' else '--' for trg in sorted(lang2lang)]
+        m2m_results = [f"{round(m2m[lang][trg], precision):{precision}.{precision}f}" if trg in m2m[lang]  else '--' for trg in sorted(lang2lang)]
+        print('\multirow{2}{*}{' + lang + r'} & \bloom & ' + ' & '.join(results) + r' \\')
+        print(r' & M2M & ' + ' & '.join(m2m_results) + r' \\')
+    print(r'\bottomrule')
+    print(r'\end{tabular}}')
+    print(r'\caption{Romance languages}')
+    print(r'\label{tab:flores101_summary:same-family}')
+    print('\n\n')
+    
+
+
+def table_transfer(bleu_results_file, comet_results_file):
+    model= 'bloom'
+    
+    print(r'\begin{table}[!ht]')
+    print(r'\centering\small')
+    print(r'\resizebox{\linewidth}{!}{')
+    print(r'\begin{tabular}{llrrrr}')
+    print(r'\toprule')
+    print(r'                &  & \multicolumn{2}{c}{Original} & \multicolumn{2}{c}{Truncated} \\')
+    print(r'\multicolumn{2}{l}{1-shot example direction type} & spBLEU & COMET & spBLEU & COMET \\')
+    print(r'\midrule')
+
+    def list_results(task, template):
+        bleu_data_orig = load_df(bleu_results_file, model, '', task)
+        comet_data_orig = load_df(comet_results_file, model, '', task)
+        bleu_data_trunc = load_df(bleu_results_file, model, 'newline-cut-custom-truncate', task)
+        comet_data_trunc = load_df(comet_results_file, model, 'newline-cut-custom-truncate', task)
+
+        return ["%.2f" % round(bleu_data_orig[bleu_data_orig['template'] == template]['spBLEU'].values[0], 2),
+                "%.4f" % round(comet_data_orig[comet_data_orig['template'] == template]['comet'].values[0], 4),
+                "%.2f" % round(bleu_data_trunc[bleu_data_trunc['template'] == template]['spBLEU'].values[0], 2),
+                "%.4f" % round(comet_data_trunc[comet_data_trunc['template'] == template]['comet'].values[0], 4)]
+
+    results = list_results('flores_101_mt', 'xglm-Bengali-English-source+target')
+    print(r'Same & bn$\rightarrow$en & ' + ' & '.join(results) + r' \\')
+    results = list_results('flores_101_mt_fewshot_en2bn', 'xglm-Bengali-English-source+target')
+    print(r'Opposite & en$\rightarrow$bn & ' + ' & '.join(results) + r' \\')
+    print(r'\midrule')
+    results = list_results('flores_101_mt_fewshot_hi2en', 'xglm-Bengali-English-source+target')
+    print(r'Related source & hi$\rightarrow$en & ' + ' & '.join(results) + r' \\')
+    results = list_results('flores_101_mt_fewshot_wmt_hi2en', 'xglm-Bengali-English-source+target')
+    print(r'Related source (from WMT) & hi$\rightarrow$en & ' + ' & '.join(results) + r' \\')
+    results = list_results('flores_101_mt_fewshot_fr2en', 'xglm-Bengali-English-source+target')
+    print(r'HR unrelated source & fr$\rightarrow$en & ' + ' & '.join(results) + r' \\')
+    results = list_results('flores_101_mt_fewshot_fr2ar', 'xglm-Bengali-English-source+target')
+    print(r'HR unrelated source & fr$\rightarrow$ar & ' + ' & '.join(results) + r' \\')
+    print(r'\bottomrule')
+    print(r'\end{tabular}}')
+    print(r'\caption{\label{tab:bn-en-fewshot-variation}1-shot results for Flores bn$\rightarrow$en when varying the language direction of 1-shot examples. HR=high-resource.}')
+    print(r'\end{table}')
+
+    
 # print out all tables from this results file
-results_file = 'outputs/flores-101/1-shot/bleu-results.tsv'
-table_romance_langs(results_file)
-table_lowresource(results_file)
-table_midresource(results_file)
-table_highresource(results_file)
+bleu_results_file = 'outputs/flores-101/1-shot/bleu-results.tsv'
+comet_results_file = 'outputs/flores-101/1-shot/comet-results.tsv'
+
+# choose one of these and comment the other
+results_file = bleu_results_file
+#results_file = comet_results_file
+
+# choose which type of postprocessing to include (original or truncated)
+postproc='' # original
+#postproc='newline-cut-custom-truncate' # truncated
+
+# print all tables
+table_highresource(results_file, postproc=postproc)
+table_midresource(results_file, postproc=postproc)
+
+table_lowresource(results_file, postproc=postproc)
+table_romance_langs(results_file, postproc=postproc)
+
+# this table always uses both original and truncated for both comet and bleu so they are not specified
+table_transfer(bleu_results_file, comet_results_file)
